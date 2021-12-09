047e8829cea9869e45b82c1d3f1aefe87a340300
YatinderEspire <Yatinder.singh@espire.com>
2021-12-09 18:15:00 +0530

    Changes in producer

--- library/src/main/java/com/sniffer/App.java
+++ library/src/main/java/com/sniffer/App.java
@@ -4,14 +4,9 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
-		/*
-		 * Consumer consumer1 = new Consumer(); Consumer consumer2 = new Consumer();
-		 * Consumer consumer3 = new Consumer(); Consumer consumer4 = new Consumer();
-		 */
+		
         producer.start();
-		/*
-		 * consumer1.start(); consumer2.start(); consumer3.start(); consumer4.start();
-		 */
+		
     }
 }
 
--- library/src/main/java/com/sniffer/Producer.java
+++ library/src/main/java/com/sniffer/Producer.java
@@ -17,54 +17,50 @@ import com.sniffer.utils.ProcessingThread;
 
 public class Producer extends ProcessingThread {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
 
-    private static Integer instance = 0;
-    private final Integer THROUGHPUT = 100;
+	private static Integer instance = 0;
+	private final Integer THROUGHPUT = 100;
 
-    private ConcurrentSynchronizeQueue queue = null;
-    private Handler handler = null;
-    private Tenant tenant = null;
-    private ThrottleImpl timer = null;
+	private ConcurrentSynchronizeQueue queue = null;
+	private Handler handler = null;
+	private Tenant tenant = null;
+	private ThrottleImpl timer = null;
 
-    public Producer() {
-        queue = ConcurrentSynchronizeQueue.getInstance();
-        handler = new Handler();
-        tenant = new Tenant("Producer" + ++instance, THROUGHPUT, handler);
-        timer = new ThrottleImpl(THROUGHPUT, tenant, handler);
-        
-        timer.start();
-    }
+	public Producer() {
+		queue = ConcurrentSynchronizeQueue.getInstance();
+		handler = new Handler();
+		tenant = new Tenant("Producer" + ++instance, THROUGHPUT, handler);
+		timer = new ThrottleImpl(THROUGHPUT, tenant, handler);
 
+		timer.start();
+	}
 
-    @Override
-    public void process() {
-        String s = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-        try {
-        	
-        	
-        	
-            if (handler.getCount(tenant.getName()) < THROUGHPUT) {
-            	long startTime=System.currentTimeMillis();
-            	System.err.println(startTime+"  start");
-                queue.enqueue(HexUtil.toHexString(s.getBytes()));
-                handler.incrementCount(tenant.getName());
-                long endTime=System.currentTimeMillis();
-                
-                System.err.println(endTime+"  end");
-                System.err.println(endTime-startTime);
-               long l=(1000/THROUGHPUT)-(endTime-startTime);
-              System.err.println(l);
-             
-                Thread.sleep(6);
-                System.err.println(queue.size());
-            } else {
-                Thread.sleep(10);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    
-	
+	@Override
+	public void process() {
+		String s = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+		try {
+
+			if (handler.getCount(tenant.getName()) < THROUGHPUT) {
+				long startTime = System.currentTimeMillis();
+				System.err.println(startTime + "  start");
+				queue.enqueue(HexUtil.toHexString(s.getBytes()));
+				handler.incrementCount(tenant.getName());
+				long endTime = System.currentTimeMillis();
+
+				System.err.println(endTime + "  end");
+				System.err.println(endTime - startTime);
+				long l = (1000 / THROUGHPUT) - (endTime - startTime);
+				System.err.println(l);
+
+				Thread.sleep(6);
+				System.err.println(queue.size());
+			} else {
+				Thread.sleep(10);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
